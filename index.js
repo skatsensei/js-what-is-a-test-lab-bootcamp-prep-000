ed98f5674f177366f4ef8ee79fb9569eabbf1928
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2017-10-29 03:34:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ var message = `${name} is ${height} inches tall`
 module.exports = {name, height, message}
 var word = "bird"
 console.log(word)
+var a = 5, b = 2, c = 3, d = "hello", e = "goodbye"
+console,log(a+b)
